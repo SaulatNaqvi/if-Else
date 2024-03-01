4fe06d913ea235d228681b581a1159a1ecd8c824
SaulatNaqvi <157523160+SaulatNaqvi@users.noreply.github.com>
2024-03-02 00:54:34 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,327 @@
+/*Assignment If-Else*/
+
+/*document.write("<h1>Assignment If-Else Question No:1</h1>")
+var user_num=+prompt("Enter your number")
+var given_num=3
+
+remdr=user_num%given_num
+
+
+if
+(remdr == 0)
+{
+document.write("<h3>The given number is divisible by :- 3</h3>")
+}
+else
+{
+    document.write("<h3>Number is not divisible by :- 3</h3>")
+}*/
+
+/*document.write("<h1>Assignment If-Else Question No:2</h1>")
+var num=+prompt("Enter your number")
+var even=num%2
+
+if
+(even == 0)
+{
+document.write("<h3>Number is an Even number</h3>")
+}
+
+else
+{
+    document.write("<h3>Number is an Odd number</h3>")
+}*/
+
+/*document.write("<h1>Assignment If-Else Question No:3</h1>")
+var user_age = +prompt("Enter your age")
+if
+(user_age >=18)
+{
+document.write('<h3>"Old Enough"')
+}
+else
+{
+    document.write('<h3>Too Young</h3>')
+}*/
+
+/*document.write("<h1>Assignment If-Else Question No:4</h1>")
+var user_nam = prompt("Enter your name")
+var my_nam="saulat"
+
+if
+(user_nam == my_nam)
+{
+    alert("You are Special")
+}*/
+
+
+/*document.write("<h1>Assignment If-Else Question No:6</h1>")
+var str_num = prompt("Enter number or word ")
+var uc= str_num .toUpperCase()
+
+if
+(str_num == uc)
+{
+    document.write("<h3>The String is Upper-Case</h3>")
+}
+
+else
+{
+    document.write("<h3>The String is Lower-Case</h3>")
+}*/
+
+/*document.write("<h1>Assignment If-Else Question No:7</h1>")
+
+var user_num1 = +prompt("Enter 1st number")
+var user_num2 = +prompt("Enter 2nd number")
+var oper = prompt("Select any +,-,/,*,%")
+
+if
+(oper == "+")
+{
+    document.write("<h3>The addition of two inputs are:</h3>", user_num1+user_num2)
+}
+
+else if
+(oper == "-")
+{
+    document.write("<h3>The addition of two inputs are:</h3>", user_num1-user_num2)
+}
+
+
+else if
+(oper == "*")
+{
+    document.write("<h3>The addition of two inputs are:</h3>", user_num1*user_num2)
+}
+
+
+else if
+(oper == "/")
+{
+    document.write("<h3>The addition of two inputs are:</h3>", user_num1/user_num2)
+}
+
+else if
+(oper == "%")
+{
+    document.write("<h3>The addition of two inputs are:</h3>", user_num1%user_num2)
+}*/
+
+/*document.write("<h1>Assignment If-Else Question No:8</h1>")
+
+var time=+prompt ("choose timings")
+
+if
+(time >= 0000 && time <1200)
+alert("Good Morning!")
+
+else if
+(time >=1200 && time <1700)
+alert("Good Afternoon!")
+
+else if
+(time >=1700 && time<2100)
+alert("Good Evening!")
+
+else if
+(time >=2100 && time<=2359)
+alert("Good night!")*/
+
+
+/*document.write("<h1>Assignment If-Else Question No:9</h1>")
+var user_year = + prompt("Enter Year in 4 Digits")
+var leap_year = user_year%2
+document.write(leap_year)
+
+if
+(leap_year == 0)
+
+alert("Congrats!! its a Leap year")*/
+
+/*document.write("<h1>Assignment If-Else Question No:10</h1>")
+var user_pass= +prompt("Enter Password")
+var real_pass= 1234
+
+if
+(user_pass == " ")
+
+document.write("Please Enter Password")
+
+{
+    if
+    (user_pass == real_pass)
+
+{
+    document.write("<h3>Your Password is Correct</h3>")
+}
+else
+
+{
+    document.write("<h3>Your Password is In-correct</h3>")
+}
+
+}*/
+
+
+/*document.write("<h1>Assignment If-Else Question No:11</h1>")
+
+var input_nam = prompt("Enter your name Mr.Fahad")
+var fhd_nam = "fahad"
+
+if
+(input_nam == fhd_nam)
+{
+    document.write("<h2>Hello Fahad</h2>")
+
+}
+
+else
+alert ("You are not Fahad")*/
+
+
+/*document.write("<h1>Assignment If-Else Question No:12</h1>")
+
+var hour = +prompt("Enter an hour")
+
+if (hour < 18) 
+{
+alert("Good day")
+}
+else
+{
+alert("Good evening")
+}*/
+
+/*document.write("<h1>Assignment If-Else Question No:13</h1>")
+var int1 = +prompt("Enter Integer First")
+var int2 = +prompt("Enter Integer Second")
+
+if
+(int1 > int2)
+{
+document.write("<h3>Int1 is Greater then Int2</h3>")
+}
+else if
+(int2 > int1)
+{
+    document.write("<h3>Int2 is Greater then Int1</h3>")
+}
+
+else
+{
+    document.write("<h3>Both integers are equal</h3>")
+}*/
+
+/*----document.write("<h1>Assignment If-Else Question No:14</h1>")
+
+var num = +prompt("Enter your number")
+
+if
+(num > 0)
+{
+    document.write("<h3>The number is Positive</h3>")
+}
+
+else if
+(num < 0)
+{
+   document.write("<h3>The number is Negative</h3>") 
+}
+
+else if
+(num == 0)
+{
+   document.write("<h3>The number is ZERO</h3>") 
+}
+
+else
+{
+    document.write("<h3>TRY AGAIN</h3>")
+}*/
+
+
+
+
+/*document.write("<h1>Assignment If-Else Question No:15</h1>")
+var time = +prompt ("What time is it,Sir!")
+
+if 
+(time == 6 || time <= 9)
+{
+document.write("<h3>Breakfast is served</h3>")
+}
+
+else if 
+(time == 11 || time <= 13)
+{
+document.write("<h3>Time for lunch</h3>")
+}
+
+else if 
+(time >= 17 && time <= 20)
+{
+document.write("<h3>Its Dinner Time</h3>")
+}
+
+else
+{
+ document.write("<h3>Sorry you'll have to wait or go get a snack</h3>")
+}*/
+
+/*document.write("<h1>Assignment If-Else Question No:16</h1>")
+
+var x = 1234
+var y = "saulat"
+var z;
+
+console.log(typeof x)*/
+
+
+/*var x = +prompt("Enter value")
+
+if (typeof (x) === "number") 
+{
+    document.write("x is a number")
+}
+
+
+    else if (typeof (x) === string)
+    { 
+        document.write("x is a string")
+    }*/
+       
+    
+    
+
+
+
+
+/*-----document.write("<h1>Assignment If-Else Question No:17</h1>")
+var vow = prompt("Enter one Vowel at a time")
+vow = a
+vow = e  
+
+if
+(vow == a || vow == e)
+{
+    document.write("Its Vowel")
+}*/
+
+/*document.write("<h1>Assignment If-Else Question No:18</h1>")
+
+var num=10
+var num1=8
+if
+(num !=num1)
+{
+    document.write("<h3> 10 is NOT equal to 8</h3>")
+}*/
+
+
+/*-- SWITCH STATEMENTdocument.write("<h1>Assignment If-Else Question No:19</h1>")*/
+
+
+/*----(ternary) operatordocument.write("<h1>Assignment If-Else Question No:20</h1>")*/
+
+/*5,19,20*/ /*16,17   */
